7d3ea0381ae5872ddf49ece22f5df2b6db2c780c
Davi Scholl <davis@bandavide.com.br>
2020-10-16 01:06:54 -0300

    Resolucao exercicio 7

--- 07_front_back.py
+++ 07_front_back.py
@@ -3,19 +3,40 @@
 
 Considere dividir uma string em duas metades.
 Caso o comprimento seja par, a metade da frente e de trás tem o mesmo tamanho.
-Caso o comprimento seja impar, o caracter extra fica na metade da frente.
+Caso o comprimento seja ímpar, o caracter extra fica na metade da frente.
 
 Exemplo: 'abcde', a metade da frente é 'abc' e a de trás é 'de'.
 
 Finalmente, dadas duas strings a e b, retorne uma string na forma:
 a-frente + b-frente + a-trás + b-trás
 """
+
+
 def front_back(a, b):
-    # +++ SUA SOLUÇÃO +++
-    return
+    """
+    if len(a) % 2 == 0:
+        a_frente = a[:int(len(a) / 2)]
+        a_tras = a[int(len(a) / 2):]
+    else:
+        a_frente = a[:int(len(a) / 2 + 0.5)]
+        a_tras = a[int(len(a) / 2 + 0.5):]
+
+    if len(b) % 2 == 0:
+        b_frente = b[:int(len(b) / 2)]
+        b_tras = b[int(len(b) / 2):]
+    else:
+        b_frente = b[:int(len(b) / 2 + 0.5)]
+        b_tras = b[int(len(b) / 2 + 0.5):]
+
+    return ''.join([a_frente, b_frente, a_tras, b_tras])
+    """
+
+    a1, a2 = a[:len(a) // 2 + len(a) % 2], a[len(a) // 2 + len(a) % 2:]
+    b1, b2 = b[:len(b) // 2 + len(b) % 2], b[len(b) // 2 + len(b) % 2:]
+    return ''.join([a1, b1, a2, b2])
 
 
-# --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
+# --- Daqui para baixo são apenas códigos auxiliares de teste. ---
 
 def test(f, in_, expected):
     """
