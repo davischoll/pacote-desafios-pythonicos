6c33a2a318bc060654d5d4124cd4adfd7e9b3fb0
Davi Scholl <davis@bandavide.com.br>
2020-10-16 23:33:14 -0300

    Resolucao exercicio 9

--- 09_front_x.py
+++ 09_front_x.py
@@ -1,19 +1,25 @@
 """
 09. front_x
 
-Dada uma lista de strings, retorne a lista com as strings
-ordenadas, porém agrupe todas as strings que começam com 'x' primeiro.
+Dada uma lista de strings, retorne a lista com as strings ordenadas.
+Porém, agrupe todas as strings que começam com 'x' primeiro.
 
 Exemplo: ['mix', 'banana' ,'xyz', 'apple', 'xanadu', 'aardvark']
 Irá retornar: ['xanadu', 'xyz', 'aardvark', 'apple', 'banana' ,'mix']
 
-Dica: Isso pode ser resolvido criando 2 listas e ordenando cada uma
-antes de combina-las.
+Dica: Isso pode ser resolvido criando 2 listas e ordenando cada uma antes de combiná-las.
 """
 
+
 def front_x(words):
-    # +++ SUA SOLUÇÃO +++
-    return
+    s1, s2 = [], []
+    for w in words:
+        if w[0] == 'x':
+            s1.append(w)
+        else:
+            s2.append(w)
+    s1.sort(), s2.sort()
+    return s1 + s2
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
